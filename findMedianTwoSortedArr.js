490eee54c70f53a97a8c44b85cb21621b1df0bab
grace wingo <37881553+gracewingo@users.noreply.github.com>
2019-10-23 21:07:12 -0400

    editing description

--- findMedianTwoSortedArr.js
+++ findMedianTwoSortedArr.js
@@ -1,7 +1,17 @@
-//Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
-//median is the number in the middle 
+/*
+https://leetcode.com/problems/median-of-two-sorted-arrays/submissions/
+Description: 
 
-//https://leetcode.com/problems/median-of-two-sorted-arrays/submissions/
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n))
+You may assume nums1 and nums2 cannot be both empty.
+
+Challenge successfully passed the test cases. 
+
+Details: Runtime: 208 ms, faster than 5.43% of JavaScript online submissions for Median of Two Sorted Arrays.
+Memory Usage: 44.8 MB, less than 8.51% of JavaScript online submissions for Median of Two Sorted Arrays.
+*/
 
 var findMedianSortedArrays = function(nums1, nums2) {
     //merge the two arrays together 
