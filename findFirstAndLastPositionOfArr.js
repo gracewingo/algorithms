345ce09e3b4fb37f7fa09401ef335581b0129fdc
grace wingo <37881553+gracewingo@users.noreply.github.com>
2019-10-24 21:29:24 -0400

    new committ

---
+++ findFirstAndLastPositionOfArr.js
@@ -0,0 +1,28 @@
+/*
+Given an array of integers nums sorted in ascending order
+Find the starting and ending position of a given target value.
+Your algorithm's runtime complexity must be in the order of O(log n).
+If the target is not found in the array, return [-1, -1].
+*/
+
+var searchRange = function(nums, target) {
+    	var arr = [];
+    	var idx1, idx2;
+    	if (nums.indexOf(target) !== -1){
+    		nums.map(function(el){
+    			if (el === target){
+    				idx1 = nums.indexOf(el);
+    				idx2 = nums.lastIndexOf(el);
+    			}
+    		})
+    		arr.push(idx1,idx2)
+    	} else {
+            //TO DO: try with map and filter, and make the else not hard coded 
+    		arr = [-1,-1];
+    	}
+  	return arr;
+};
+
+var nums = [5,7,7,8,8,10], target = 6;
+
+console.log(searchRange(nums, target))
